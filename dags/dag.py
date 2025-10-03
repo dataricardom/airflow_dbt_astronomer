aee7b697ff80da2399656cbbc74ddbb5c29556bd
Ricardo Marques <dataricardom@gmail.com>
2025-10-02 22:00:41 -0300

    refact: dags com ambiente dev e prod

--- dags/dag.py
+++ dags/dag.py
@@ -1,9 +1,10 @@
+from airflow.models import Variable
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import PostgresUserPasswordProfileMapping
 import os
 from pendulum import datetime
 
-profile_config = ProfileConfig(
+profile_config_dev = ProfileConfig(
         profile_name="dbt_dw",
         target_name="dev",
         profile_mapping=PostgresUserPasswordProfileMapping(
@@ -13,11 +14,28 @@ profile_config = ProfileConfig(
 
 )
 
+
+profile_config_prod = ProfileConfig(
+        profile_name="dbt_dw",
+        target_name="prod",
+        profile_mapping=PostgresUserPasswordProfileMapping(
+                conn_id="raiway_postgres_db",
+                profile_args={"schema": "public"}
+        ),
+
+)
+
+dbt_env = Variable.get("dbt_env", default_var="dev").lower()
+if dbt_env not in ("dev","prod"):
+    raise ValueError(f"dbt_env inválido: {dbt_env!r}, use 'dev ou 'prod'")
+
+profile_config = profile_config_dev if dbt_env == "dev" else profile_config_prod
+
 my_cosmos_dag = DbtDag(
 
     project_config=ProjectConfig(
-        "/usr/local/airflow/dbt/dbt_dw",
-    
+        dbt_project_path= "/usr/local/airflow/dbt/dbt_dw",
+        project_name="dbt_dw",
     ),
     
     profile_config=profile_config,
@@ -32,8 +50,8 @@ my_cosmos_dag = DbtDag(
     },
 
     schedule="@daily",
-    start_date=datetime(2025,9,23),
+    start_date=datetime(2025,10,2),
     catchup=False,
-    dag_id="dag_dbt_dw",
+    dag_id=f"dag_dbt_dw_{dbt_env}",
     default_args={"retries": 2},
 )
